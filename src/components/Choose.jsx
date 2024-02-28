d7f78f1c6e562eaaf8abb02fe4b2efe54ef8bd45
Selormey Hubert <selormeyh@gmail.com>
2024-02-28 14:37:04 +0000

    Update Choose.jsx: Improve the 'Why Choose FarmHand?' section

--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -21,44 +21,108 @@ const Choose = () => {
         </div>
       ) : (
         <>
-          
           <div className="md:min-h-screen flex flex-col-reverse md:flex-row items-center gap-5 md:mx-5 mx-5 mt-5 ">
             <div className="w-full md:w-2/3 mb-2 mt-5 relative ">
-              
               <img
                 src={farm2}
                 alt="farm2"
-                style={{ width: "100%", maxHeight: "300px", objectFit: "cover" }}
+                style={{
+                  width: "100%",
+                  maxHeight: "300px",
+                  objectFit: "cover",
+                }}
               />
               <img
                 src={farm1}
                 alt="farm1"
-                style={{ width: "100%", maxHeight: "300px", objectFit: "cover", marginTop: "10px" }}
+                style={{
+                  width: "100%",
+                  maxHeight: "300px",
+                  objectFit: "cover",
+                  marginTop: "10px",
+                }}
               />
             </div>
 
             <div className="w-full md:w-3/4 text-center space-y-2 mb-4 text-left">
               <div className="text-left space-y-2 mb-2">
-              <h1 className="text-black mb-10 text-center text-4xl font-bold font-quicksand">
-            Why Choose <span className="text-green">FarmHand?</span>
-          </h1>
+                <h1 className="text-black mb-10 text-center text-4xl font-bold font-quicksand">
+                  Why Choose <span className="text-green">FarmHand?</span>
+                </h1>
                 <ul style={{ listStyleType: "none", padding: 0 }}>
-                  <li style={{ padding: "10px", borderRadius: "8px", marginBottom: "40px", color: "black", display: "flex", alignItems: "left" }}>
+                  <li
+                    style={{
+                      padding: "10px",
+                      borderRadius: "8px",
+                      marginBottom: "40px",
+                      color: "black",
+                      display: "flex",
+                      alignItems: "left",
+                    }}
+                  >
                     <span style={{ marginRight: "10px" }}>✔</span>
-                    <strong style={{ marginRight: "10px" }}>Carefully Sourced Talent:</strong> FarmHand provides access to carefully sourced and vetted farm professionals, ensuring reliability and quality in every hire.
+                    <strong style={{ marginRight: "10px" }}>
+                      Carefully Sourced Talent:
+                    </strong>{" "}
+                    FarmHand provides access to carefully sourced and vetted
+                    farm professionals, ensuring reliability and quality in
+                    every hire.
                   </li>
-                  <li style={{ padding: "5px", borderRadius: "8px", marginBottom: "40px", color: "black", display: "flex", alignItems: "left" }}>
+                  <li
+                    style={{
+                      padding: "5px",
+                      borderRadius: "8px",
+                      marginBottom: "40px",
+                      color: "black",
+                      display: "flex",
+                      alignItems: "left",
+                    }}
+                  >
                     <span style={{ marginRight: "10px" }}>✔</span>
-                    <strong style={{ marginRight: "10px" }}>Streamlined Recruitment:</strong> Simplify the process of hiring quality laborers with FarmHand, alleviating the stress and time drain of recruitment.
+                    <strong style={{ marginRight: "10px" }}>
+                      Streamlined Recruitment:
+                    </strong>{" "}
+                    Simplify the process of hiring quality laborers with
+                    FarmHand, alleviating the stress and time drain of
+                    recruitment.
                   </li>
-                  <li style={{ padding: "5px", borderRadius: "8px", marginBottom: "40px", color: "black", display: "flex", alignItems: "left" }}>
+                  <li
+                    style={{
+                      padding: "5px",
+                      borderRadius: "8px",
+                      marginBottom: "40px",
+                      color: "black",
+                      display: "flex",
+                      alignItems: "left",
+                    }}
+                  >
                     <span style={{ marginRight: "10px" }}>✔</span>
-                    <strong style={{ marginRight: "10px" }}>Reliability in Action:</strong> Our platform connects you with reliable laborers possessing expertise to ensure timely and purpose-driven farm work completion.
+                    <strong style={{ marginRight: "10px" }}>
+                      Reliability in Action:
+                    </strong>{" "}
+                    Our platform connects you with reliable laborers possessing
+                    expertise to ensure timely and purpose-driven farm work
+                    completion.
                   </li>
-                   
-                  <li style={{ padding: "5px", borderRadius: "8px", marginBottom: "40px", color: "black", display: "flex", alignItems: "left" }}>
+
+                  <li
+                    style={{
+                      padding: "5px",
+                      borderRadius: "8px",
+                      marginBottom: "40px",
+                      color: "black",
+                      display: "flex",
+                      alignItems: "left",
+                    }}
+                  >
                     <span style={{ marginRight: "10px" }}>✔</span>
-                    <strong style={{ marginRight: "10px" }}>Comprehensive Skillsets:</strong> Our laborers are more than just workers; they are contributors. Expect a comprehensive skill set that enhances your farm's capabilities, bringing efficiency and expertise to every task.
+                    <strong style={{ marginRight: "10px" }}>
+                      Comprehensive Skillsets:
+                    </strong>{" "}
+                    Our laborers are more than just workers; they are
+                    contributors. Expect a comprehensive skill set that enhances
+                    your farm's capabilities, bringing efficiency and expertise
+                    to every task.
                   </li>
                 </ul>
               </div>
